333250defea2de2c3a079143e5f64005f97eff3c
matt maddex <mmaddex@render.com>
2024-06-13 12:17:35 -0700

    Update start.sh[skip render]

--- start.sh
+++ start.sh
@@ -6,3 +6,5 @@ echo Tailscale starting
 ./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 & ./tailscale up --authkey=${TS_AUTHKEY} --hostname=customer-ctx-caddy
 echo Tailscale started
 #caddy
+#https://github.com/caddyserver/caddy-docker/blob/645721b4b87b6c3a692641213853ce064eb82fe2/2.8/alpine/Dockerfile#L61C5-L61C83
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
