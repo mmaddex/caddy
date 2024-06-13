d14c6d4322b2ccb194b539205b26c922e5c29816
matt maddex <mmaddex@render.com>
2024-06-13 11:37:43 -0700

    Update start.sh [skip render]

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+# https://tailscale.com/kb/1190/caddy-certificates
+# https://caddy.community/t/https-in-your-vpn-caddy-now-uses-tls-certificates-from-tailscale/15380
 echo Tailscale starting
 ./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 & ./tailscale up --authkey=${TS_AUTHKEY} --hostname=customer-ctx-caddy
 echo Tailscale started
